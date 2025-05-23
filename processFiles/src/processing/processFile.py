a4a62f1cb24ba2bc8b922812e8acad99c758321e
MarcosViniSil <marcossilv203@gmail.com>
2025-05-23 11:57:25 -0300

    chore: add logs to processing files

--- processFiles/src/processing/processFile.py
+++ processFiles/src/processing/processFile.py
@@ -5,11 +5,14 @@ import boto3
 from botocore.client import Config
 import subprocess
 import shutil
+import logging
+from datetime import date
 
 load_dotenv()
 
 LOCAL_PATH = "./file"
 os.makedirs(LOCAL_PATH, exist_ok=True)
+os.makedirs("./logs", exist_ok=True)
 
 class ProcessFiles:
 
@@ -19,83 +22,111 @@ class ProcessFiles:
         self.fileName = None
 
     def getMessageFromQueue(self, message: str) -> None:
+        self.configureFileLog()
         message = message.replace("'", '"')
         try:
             data = json.loads(message)
             videoId = str(data["videoId"])
             bucketName = str(data["videoUrl"]).split("/")[-2]
             fileName = str(data["videoUrl"]).split("/")[-1]
-            
-            pathDownload = self.downloadFileFromBucket(bucketName,fileName)
-            streamFolderName = self.convertVideoToStream(fileName,pathDownload)
 
-            self.sendStreamToBucket(streamFolderName,bucketName,videoId)
+            pathDownload = self.downloadFileFromBucket(bucketName, fileName)
+            streamFolderName = self.convertVideoToStream(fileName, pathDownload)
+
+            self.sendStreamToBucket(streamFolderName, bucketName, videoId)
 
         except json.JSONDecodeError as e:
-            print(f"Erro ao decodificar a mensagem: {e}")
+            print(f"Erro ao decodificar a mensagem: {e.msg}")
+        except ValueError as v:
+            print(f"Erro: {e.msg}")
+        except Exception as r:
+            print(f"Erro: {r}")
 
-
-
-    def convertVideoToStream(self,nameFile:str,pathDownload:str) -> str:
+    def convertVideoToStream(self, nameFile: str, pathDownload: str) -> str:
         nameFolder = nameFile
         if nameFile.endswith(".mp4"):
             nameFolder = os.path.splitext(nameFile)[0]
-        
+
         os.makedirs(f"./{nameFolder}", exist_ok=True)
-        #ffmpeg -i input.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls caminho/para/pasta/output.m3u8
-        resultado = subprocess.run(["ffmpeg", "-i",f"{pathDownload}","-codec:","copy",
-                                    "-start_number","0","-hls_time","10",
-                                    "-hls_list_size","0","-f","hls",f"./{nameFolder}/output.m3u8"], 
-                                   capture_output=True, text=True)
+        outputPath = f"./{nameFolder}/output.m3u8"
         
-        #shutil.rmtree(f"./{nameFolder}")
+        cmd = ["ffmpeg","-i",pathDownload,"-vf","scale=854:480",
+                "-c:v","libx264","-b:v","1000k","-c:a","aac",
+                "-b:a","96k","-hls_time","10","-hls_list_size","0","-f","hls",outputPath,]
+        
+        logging.info(f"Iniciando conversão de {pathDownload} para 480p.")
+
+        resultado = subprocess.run(cmd, capture_output=True, text=True)
+
+        # shutil.rmtree(f"./{nameFolder}")
+
+        if resultado.returncode != 0:
+            logging.error(f"Erro na conversão, erro que ocorreu: {resultado.stderr}")
+            logging.error(resultado.stderr)
+            raise ValueError("Ocorreu um erro ao converter o vídeo em streaming, tente novamente")
+        else:
+            logging.info(f"Conversão bem-sucedida. Arquivo HLS: {outputPath}")
 
         return nameFolder
-        #print(resultado.stdout)
-        #print(resultado.stderr)
 
-    def sendStreamToBucket(self,folder_path:str,bucket_name:str,videoId: str):
+    def sendStreamToBucket(self, folder_path: str, bucket_name: str, videoId: str):
+        logging.info(f"Iniciando envio de stream de id {videoId} que está na pasta local {folder_path} para o bucket {bucket_name}")
+        s3 = self.createConnection()
+
         try:
-            s3 = self.createConnection()
             for root, _, files in os.walk(folder_path):
-               for file in files:
-                   local_path = os.path.join(root, file)
-                   relative_path = os.path.relpath(local_path, folder_path).replace("\\", "/")
+                for file in files:
+                    local_path = os.path.join(root, file)
+                    relative_path = os.path.relpath(local_path, folder_path).replace("\\", "/")
 
-                   s3_key = f"{videoId}/{relative_path}" 
+                    s3_key = f"{videoId}/{relative_path}"
 
-                   s3.upload_file(local_path, bucket_name, s3_key)
-                   
-                   print(f"Enviado: {local_path} → {bucket_name}/{s3_key}")
+                    s3.upload_file(local_path, bucket_name, s3_key)
+
+            logging.info(f"stream de id {videoId} enviado com sucesso para o bucket {bucket_name}")
+
         except Exception as e:
-            print(e)        
+            logging.error(f"ocorreu um erro ao enviar o stream de id {videoId} para o bucket {bucket_name}. Erro que ocorreu: {e}")
+            raise ValueError("Ocorreu um erro ao salvar o vídeo em nuvem, tente novamente")
 
-    def downloadFileFromBucket(self,bucketName:str,fileName: str) -> str:
+    def downloadFileFromBucket(self, bucketName: str, fileName: str) -> str:
         if fileName == "" or bucketName == "":
-            print("Arquivo ou bucket inválido.")
-            return
+            raise ValueError("Ocorreu um erro ao processar o vídeo recebido, tente novamente")
 
-        try:
-            s3 = self.createConnection()
-            local_path = os.path.join(LOCAL_PATH, fileName)
-            s3.download_file(bucketName, fileName, local_path)
-            return local_path
-
-        except Exception as e:
-            print(f"Erro ao baixar o arquivo: {e}")
-
+        s3 = self.createConnection()
+        local_path = os.path.join(LOCAL_PATH, fileName)
+        s3.download_file(bucketName, fileName, local_path)
+        return local_path
 
     def createConnection(self):
         try:
             s3 = boto3.client(
-            "s3",
-            endpoint_url='http://localhost:9000',
-            aws_access_key_id=os.environ["ACCESS_KEY_AWS"],
-            aws_secret_access_key=os.environ["SECRET_KEY_AWS"],
-            aws_session_token=None,
-            config=boto3.session.Config(signature_version='s3v4'),
-            verify=False
+                "s3",
+                endpoint_url="http://localhost:9000",
+                aws_access_key_id=os.environ["ACCESS_KEY_AWS"],
+                aws_secret_access_key=os.environ["SECRET_KEY_AWS"],
+                aws_session_token=None,
+                config=boto3.session.Config(signature_version="s3v4"),
+                verify=False,
             )
             return s3
         except Exception as e:
-            raise ValueError("Erro ao se conectar com o S3: " + str(e))
+            raise ValueError("Ocorreu um erro interno ao tentar processar o vídeo, tente novamente")
+
+    def configureFileLog(self):
+        dateActual = date.today()
+        foundFile = False
+
+        for root, _, files in os.walk("./logs"):
+            for file in files:
+                if str(file).endswith(".log"):
+                    if(os.path.splitext(str(file))[0] == dateActual):
+                        foundFile = True
+
+        if not foundFile:
+            logging.basicConfig(
+                level=logging.INFO,
+                format="%(asctime)s - %(levelname)s - %(message)s",
+                filename=f"./logs/{dateActual}.log",
+                filemode="a",
+            )
