f340447f2aee13c3ca51b7c4038d3f07ef96226d
MarcosViniSil <marcossilv203@gmail.com>
2025-06-01 16:32:42 -0300

    feat: add router to navigate between pages

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import VideoUpload from './pages/upload/videoUpload'
+import ReceiveVideo from './components/recieveVideo/ReceiveVideo'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <VideoUpload/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ReceiveVideo />} />
+        <Route path="/perfil" element={<ReceiveVideo />} />
+        <Route path="/historico" element={<ReceiveVideo />} />
+        <Route path="/upload" element={<VideoUpload />} />
+        <Route path="*" element={<VideoUpload />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
